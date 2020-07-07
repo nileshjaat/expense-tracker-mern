df0ac18f425f141780c6be79a539001da05cd952
Nilesh <nileshjaat341@gmail.com>
2020-07-07 14:05:38 +0530

    initial commit

--- client/src/components/Transaction.js
+++ client/src/components/Transaction.js
@@ -1,4 +1,5 @@
 import React, { useContext } from "react";
+import { numberWithCommas } from "../utils/format";
 
 import { GlobalContext } from "../context/GlobalState";
 
@@ -11,11 +12,11 @@ export const Transaction = ({ transaction }) => {
     <li className={transaction.amount < 0 ? "minus" : "plus"}>
       {transaction.text}{" "}
       <span>
-        {sign}₹{Math.abs(transaction.amount)}
+        {sign}₹{numberWithCommas(Math.abs(transaction.amount))}
       </span>
       <button
         className="delete-btn"
-        onClick={() => deleteTransaction(transaction.id)}
+        onClick={() => deleteTransaction(transaction._id)}
       >
         x
       </button>
